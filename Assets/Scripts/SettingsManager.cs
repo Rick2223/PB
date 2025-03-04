da55371968511c85f8e938553691c5bcbe8ef45a
Rick2223 <Rickvanrooijen1512@outlook.com>
2025-03-04 21:34:08 +0100

    main menu UI, in-game menu UI, saving volume values

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static MainMenuSaveManager;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; set; }
+
+    public Button BackButton;
+
+    public Slider masterSlider;
+    public GameObject masterValue;
+
+    public Slider musicSlider;
+    public GameObject musicValue;
+
+    public Slider effectsSlider;
+    public GameObject effectsValue;
+
+    private void Start()
+    {
+        BackButton.onClick.AddListener(()=>
+        {
+            MainMenuSaveManager.Instance.SaveVolumeSettings(musicSlider.value, effectsSlider.value, masterSlider.value);
+
+            print("Save to Player Pref");
+        });
+
+        StartCoroutine(LoadAndApplySettings());
+    }
+
+    private IEnumerator LoadAndApplySettings()
+    {
+        LoadAndSetVolume();
+
+        // Load GrapicsSettings
+        // Load Key Bindinds
+
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void LoadAndSetVolume()
+    {
+        VolumeSettings volumeSettings = MainMenuSaveManager.Instance.LoadVolumeSettings();
+
+        masterSlider.value = volumeSettings.master;
+        musicSlider.value = volumeSettings.music;
+        effectsSlider.value = volumeSettings.effects;
+
+        print("Volume Settings are loaded");
+
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        masterValue.GetComponent<TextMeshProUGUI>().text = "" + (masterSlider.value) + "";
+        musicValue.GetComponent<TextMeshProUGUI>().text = "" + (musicSlider.value) + "";
+        effectsValue.GetComponent<TextMeshProUGUI>().text = "" + (effectsSlider.value) + "";
+    }
+
+
+}
